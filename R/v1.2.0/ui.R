f4e19e755b5a1a5c8a3ec2bfd255c8c04ab269a4
Myo Minn Oo <myominnoo@users.noreply.github.com>
2022-11-22 08:21:17 -0600

    add favicon.ico

--- R/v1.2.0/ui.R
+++ R/v1.2.0/ui.R
@@ -2,6 +2,13 @@
 
 ui <- fluidPage(
   theme = bslib::bs_theme(5, "sketchy"),
+  # title = "Budget Manager", 
+  titlePanel(
+    windowTitle = "Title that appears in the browser bar",
+    title = tags$head(tags$link(rel="icon", 
+                                href="data:www;base64/favicon.ico", 
+                                type="image/x-icon")
+    )),
   tabsetPanel(
     id = "tab",
     type = "hidden",
